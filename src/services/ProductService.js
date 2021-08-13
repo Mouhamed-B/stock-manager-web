08ad07524790ac2f85a11be5b9cd8a4052dae2ce
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2021-08-13 00:29:43 +0000

    feat:add product service

---
+++ src/services/ProductService.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+    baseURL:'http://localhost:9000/products',
+    withCredentials:false,
+    headers:{
+        Accept: 'application/json',
+        'Content-Type':'application/json'
+    }
+})
+
+export default {
+    saveProduct(product){
+        return apiClient.post('/',product)
+    },
+    getProducts(){
+        return apiClient.get('/')
+    },
+    getProduct(id){
+        return apiClient.get('/'+id)
+    },
+    updateProduct(id, product){
+        return apiClient.put('/'+id,{
+            id:id,
+            product:product
+        })
+    },
+    deleteProduct(id){
+        return apiClient.delete('/'+id)
+    }
+}
