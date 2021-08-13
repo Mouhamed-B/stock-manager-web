2261dfc0a7f3f670fa14d936624444ea29737ae3
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2021-08-13 00:30:36 +0000

    feat:update routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,53 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import UserList from '../views/UserList.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'UserList',
+    component: UserList
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+     path: '/users/:id',
+     name: 'UserDetail',
+     props: true,
+     // route level code-splitting
+     // this generates a separate chunk (about.[hash].js) for this route
+     // which is lazy-loaded when the route is visited.
+     component: () => import( '../views/UserDetail.vue')
+  },
+  {
+    path: '/categories/',
+    name: 'CategoryList',
+    component: () => import( '../views/CategoryList.vue')
+  },
+  {
+    path: '/categories/:id',
+    name: 'CategoryDetail',
+    props: true,
+    component: () => import( '../views/CategoryDetail.vue')
+  },
+  {
+    path: '/providers/',
+    name: 'ProviderList',
+    component: () => import( '../views/ProviderList.vue')
+  },
+  {
+    path: '/providers/:id',
+    name: 'ProviderDetail',
+    props: true,
+    component: () => import( '../views/ProviderDetail.vue')
+  },
+  {
+    path: '/products/',
+    name: 'ProductList',
+    component: () => import( '../views/ProductList.vue')
+  },
+  {
+    path: '/products/:id',
+    name: 'ProductDetail',
+    props: true,
+    component: () => import( '../views/ProductDetail.vue')
   }
 ]
 
