2b07123b3033fe9387b9f5f6a95d69816e7ac234
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2021-08-13 00:25:28 +0000

    feat:add sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="nav flex-column nav-justified nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+    <router-link v-for="link in links" :key="link.short"
+     :class="{'nav-link':true}" 
+     :id="'v-pills-'+link.short+'-tab'" data-toggle="pill" 
+     @load="setActive" :to="link.route">{{link.text}}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  props: {
+    links : Array
+  },
+  data(){
+    return {
+      isActive:true
+    }
+  },
+  methods:{
+    setActive(){
+      this.isActive = false
+    }
+  }
+}
+</script>
+
