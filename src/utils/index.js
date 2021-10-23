12e9127cc1542df42b678953b999678870431ffd
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2021-10-23 15:47:16 +0000

    feat:add form edit toggle

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,6 +7,15 @@ export default {
                 id:parseInt(id)
               }
             }
+        },
+        toggleEdit(e){
+          e?.preventDefault()
+          this.disabled = !this.disabled
+          
+          document.getElementsByTagName('input').forEach(element => {
+              element.disabled = this.disabled ? 'true' : undefined
+          });
+          document.getElementById('description').disabled = this.disabled ? 'true' : undefined
         }
     }
 }
